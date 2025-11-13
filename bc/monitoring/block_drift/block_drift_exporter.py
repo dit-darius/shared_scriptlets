739b07685bba9aee3791ac99636abed295d5d10c
dit-darius <153668027+dit-darius@users.noreply.github.com>
2025-11-13 17:17:37 +0200

    Update block_drift_exporter.py

--- bc/monitoring/block_drift/block_drift_exporter.py
+++ bc/monitoring/block_drift/block_drift_exporter.py
@@ -67,7 +67,7 @@ CHAIN_CONFIGS = {
     },
     "avalanchego": {
         "local": "http://127.0.0.1:8545/ext/bc/C/rpc",
-        "remote": "https://avalanche.therpc.io",
+        "remote": "https://api.avax.network/ext/bc/C/rpc",
         "is_hex": True,
         "jsonrpc_body": {"jsonrpc": "2.0", "id": 1, "method": "eth_blockNumber", "params": []},
     }
